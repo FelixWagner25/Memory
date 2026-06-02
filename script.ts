8752cb494e331505ea1f6d21c80ec97208b7491b
Felix Wagner <post@felixwagner.eu>
2026-06-02 11:10:37 +0200

    feat: steer start button in settings component depending on user input

--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
-// let themeSelected: boolean = false;
-// let playerSelected: boolean = false;
-// let boardSelected: boolean = false;
+let themeSelected: boolean = false;
+let playerSelected: boolean = false;
+let boardSelected: boolean = false;
 
 let gameTheme: "code-vibes" | "DA-projects";
 let startPlayer: "blue" | "oragne";
@@ -8,12 +8,26 @@ let boardSize: "16cards" | "24cards" | "36cards";
 
 function setGameTheme(option:"code-vibes" | "DA-projects") {
     gameTheme = option;
+    themeSelected = true;
+    if(allSettingsSelected()) enableStartBtn();
 }
 
 function setStartPlayer(option:"blue" | "oragne") {
     startPlayer = option;
+    playerSelected = true;
+    if(allSettingsSelected()) enableStartBtn();
 }
 
 function setBoardSize(option:"16cards" | "24cards" | "36cards") {
     boardSize = option;
+    boardSelected = true;
+    if(allSettingsSelected()) enableStartBtn();
+}
+
+function allSettingsSelected() {
+    return themeSelected && playerSelected && boardSelected;
+}
+
+function enableStartBtn() {
+    document.getElementById("set-start-btn")?.classList.remove("disabled");
 }
