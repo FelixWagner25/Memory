7f58b886751d3ba3bdb22162e8f4e7128ff0a3cf
Felix Wagner <post@felixwagner.eu>
2026-06-02 12:12:45 +0200

    feat: show user selection in set panel on settings page

--- script.ts
+++ script.ts
@@ -2,32 +2,60 @@ let themeSelected: boolean = false;
 let playerSelected: boolean = false;
 let boardSelected: boolean = false;
 
-let gameTheme: "code-vibes" | "DA-projects";
-let startPlayer: "blue" | "oragne";
-let boardSize: "16cards" | "24cards" | "36cards";
+let gameTheme: "Code-vibes" | "DA-projects";
+let startPlayer: "Blue" | "Orange";
+let boardSize: "16" | "24" | "36";
 
-function setGameTheme(option:"code-vibes" | "DA-projects") {
-    gameTheme = option;
-    themeSelected = true;
-    if(allSettingsSelected()) enableStartBtn();
+function setGameTheme(option: "Code-vibes" | "DA-projects") {
+  gameTheme = option;
+  renderSetPanel(option);
+  themeSelected = true;
+  if (allSettingsSelected()) enableStartBtn();
 }
 
-function setStartPlayer(option:"blue" | "oragne") {
-    startPlayer = option;
-    playerSelected = true;
-    if(allSettingsSelected()) enableStartBtn();
+function setStartPlayer(option: "Blue" | "Orange") {
+  startPlayer = option;
+  renderSetPanel(option);
+  playerSelected = true;
+  if (allSettingsSelected()) enableStartBtn();
 }
 
-function setBoardSize(option:"16cards" | "24cards" | "36cards") {
-    boardSize = option;
-    boardSelected = true;
-    if(allSettingsSelected()) enableStartBtn();
+function setBoardSize(option: "16" | "24" | "36") {
+  boardSize = option;
+  renderSetPanel(option);
+  boardSelected = true;
+  if (allSettingsSelected()) enableStartBtn();
 }
 
 function allSettingsSelected() {
-    return themeSelected && playerSelected && boardSelected;
+  return themeSelected && playerSelected && boardSelected;
 }
 
 function enableStartBtn() {
-    document.getElementById("set-start-btn")?.classList.remove("disabled");
+  document.getElementById("set-start-btn")?.classList.remove("disabled");
+}
+
+function renderSetPanel(option: string) {
+  switch (option) {
+    case "Code-vibes":
+    case "DA-projects":
+      setInnerText("game-theme-selected", option);
+      break;
+    case "Blue":
+    case "Orange":
+      setInnerText("player-selected", option);
+      break;
+    case "16":
+    case "24":
+    case "36":
+      setInnerText("board-size-selected", option);
+      break;
+  }
+}
+
+function setInnerText(htmlId: string, text: string) {
+  const element = document.getElementById(htmlId);
+  if (element) {
+    element.innerText = text;
+  }
 }
