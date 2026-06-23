356bad9171d0310f93a8279601684cc3b8981300
Felix Wagner <post@felixwagner.eu>
2026-06-23 15:31:37 +0200

    feat: add template for game-over-screen for DA-projects theme

--- src/templates/game-over-templates.ts
+++ src/templates/game-over-templates.ts
@@ -1,7 +1,24 @@
 export function getGameOverTemplate(gameTheme: "Code-vibes" | "DA-projects") {
   switch (gameTheme) {
     case "DA-projects":
-      return ``;
+      return `
+      <div>
+        <div class="game-over__headline">Game over</div>
+        <div class="game-over-score-wrap">
+            <div>Final score</div>
+            <div class="game-over__score">
+                <div class="game-over__score--orange">
+                    <img class="player-icon-img" src="public/assets/img/chess_pawn_orange.svg" />
+                    <span id="final-score-orange">0</span>
+                </div>
+                <div class="game-over__score--blue">
+                    <img class="player-icon-img" src="public/assets/img/chess_pawn_blue.svg" />
+                    <span id="final-score-blue">0</span>
+                </div>            
+            </div>
+        </div>
+      </div>
+      `;
     default:
       return `
       <div>
